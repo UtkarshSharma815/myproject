5ffd68c2f98b736d3fba8357500fc213ec5a780d
Utkarsh Sharma <sharmautkarsh804@gmail.com>
2020-08-05 23:00:32 +0530

    Added sum module

---
+++ MyEmployeeManagement/src/com/lti/Calculator.java
@@ -0,0 +1,17 @@
+package com.lti;
+
+public class Calculator 
+{
+	public static void sum(int a, int b)
+	{
+		System.out.println("sum is "+(a+b));
+	}
+
+	public static void main(String[] args) 
+	{
+		sum(10,20);
+	
+
+	}
+
+}

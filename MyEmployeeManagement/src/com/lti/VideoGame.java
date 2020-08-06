b58a8f7e0be89bd608988bf493dd78110d6d72c1
Utkarsh Sharma <sharmautkarsh804@gmail.com>
2020-08-06 11:26:07 +0530

    added game method in video game class

--- MyEmployeeManagement/src/com/lti/VideoGame.java
+++ MyEmployeeManagement/src/com/lti/VideoGame.java
@@ -1,6 +1,11 @@
 package com.lti;
 
 public class VideoGame {
+	
+	public static void game()
+	{
+		System.out.println("pubg module");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
